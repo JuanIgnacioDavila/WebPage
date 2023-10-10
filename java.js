b5a4ed51f372c22e91452e4e8832c65ba188b17d
Juan Davila <juanidavila17@gmail.com>
2023-10-10 10:27:26 -0300

    files

---
+++ java.js
@@ -0,0 +1,8 @@
+function mostrar(bloque){
+	var caja=document.getElementById(bloque);
+	caja.style.display='block';
+}
+function ocultar(bloque){
+	var caja_off=document.getElementById(bloque);
+	caja_off.style.display='none';
+}
